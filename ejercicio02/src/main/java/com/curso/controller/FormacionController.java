1a3d7c021f6d52b12f087c84b3bfda763b19cd63
sergio lorenzo <sergio.lorenzo@altia.es>
2021-10-11 17:58:13 +0200

    Ejercicio 2

---
+++ ejercicio02/src/main/java/com/curso/controller/FormacionController.java
@@ -0,0 +1,33 @@
+package com.curso.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.model.Formacion;
+import com.curso.service.FormacionService;
+
+@RestController
+public class FormacionController {
+
+	@Autowired
+	FormacionService formacionService;
+	
+	@PostMapping(value="formacion", consumes=MediaType.APPLICATION_JSON_VALUE)
+	public void altaCurso (@RequestBody Formacion formacion) {
+		formacionService.altaFormacion(formacion);
+	}
+	
+	@GetMapping(value="formaciones", produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Formacion> buscarCursos () {
+		return formacionService.buscarFormaciones();
+	}
+}
